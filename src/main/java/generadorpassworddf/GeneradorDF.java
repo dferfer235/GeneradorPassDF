68bd5d21c30c05a3eca54e51360ddae1c22f4f2d
dferfer235 <dferfer235@g.educaand.es>
2021-11-17 14:06:55 +0100

    Inicio del proyecto

---
+++ src/main/java/generadorpassworddf/GeneradorDF.java
@@ -0,0 +1,15 @@
+
+package generadorpassworddf;
+
+public class GeneradorDF {
+
+    //Condiciones de la contraseña
+    //4 numeros distintos del 0 al 9
+    //La suma de los digitos 1 y 4 son inpares
+    //La multiplicacion de los digitos 2 y 3 es par
+    //Una letra aleatoria entre F-x
+    public static void main(String[] args) {
+        
+    }
+
+}
