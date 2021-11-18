f78df835bae620a48517d96c4f481e926ab69402
dferfer235 <dferfer235@g.educaand.es>
2021-11-18 10:10:23 +0100

    Generador

--- src/main/java/generadorpassworddf/GeneradorDF.java
+++ src/main/java/generadorpassworddf/GeneradorDF.java
@@ -1,5 +1,6 @@
-
 package generadorpassworddf;
+
+import java.util.Random;
 
 public class GeneradorDF {
 
@@ -9,7 +10,35 @@ public class GeneradorDF {
     //La multiplicacion de los digitos 2 y 3 es par
     //Una letra aleatoria entre F-x
     public static void main(String[] args) {
-        
+        int n1 = 0, n2, n3, n4 = 0;
+        int suma, multiplicacion;
+        char letra;
+
+        Random numAleatorio = new Random();
+
+        do {
+            letra = (char) (numAleatorio.nextInt(88 - 70 + 1) + 70);
+            n1 = numAleatorio.nextInt(10);
+            n2 = numAleatorio.nextInt(10);
+            n3 = numAleatorio.nextInt(10);
+            n4 = numAleatorio.nextInt(10);
+            
+            suma = n1 + n4;
+            if (suma % 2 != 1) {
+                n1 = n4;
+                continue;
+            }
+
+            multiplicacion = n2 * n3;
+            if (multiplicacion % 2 != 0) {
+                n2 = n3;
+                continue;
+            }
+
+        } while (n1 == n2 || n1 == n3 || n1 == n4 || n2 == n3 || n2 == n4 || n3 == n4);
+
+        System.out.println("-------------------------");
+        System.out.println("Tu contraseña es: " + n1 + n2 + n3 + n4 + letra);
     }
 
 }
